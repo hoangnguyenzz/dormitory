b42f2abf530c5b1ef565f74864cf055a42fd820f
hoangnguyenz <nguyenvanhoang2004bn@gmail.com>
2025-03-16 15:05:36 +0700

    base

---
+++ src/test/java/com/manage/quanlykytucxa/QuanlykytucxaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manage.quanlykytucxa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanlykytucxaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
