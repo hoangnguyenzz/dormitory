815cfac442e0242d36fe62ff6fc2ac06174ba8f0
hoangnguyenz <nguyenvanhoang2004bn@gmail.com>
2025-04-14 22:02:23 +0700

    14/4

--- src/main/java/com/manage/quanlykytucxa/Controller/HoaHonController.java
+++ src/main/java/com/manage/quanlykytucxa/Controller/HoaHonController.java
@@ -1,30 +1,46 @@
 package com.manage.quanlykytucxa.controller;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.manage.quanlykytucxa.domain.Hoadon;
 import com.manage.quanlykytucxa.domain.SoDienNuoc;
+import com.manage.quanlykytucxa.domain.User;
 import com.manage.quanlykytucxa.domain.response.RestResponse;
+import com.manage.quanlykytucxa.domain.response.ResultPagination;
 import com.manage.quanlykytucxa.service.HoaDonService;
+import com.turkraft.springfilter.boot.Filter;
 
 @Controller
 @RequestMapping("/api/v1/hoadon")
 public class HoaHonController {
-    
+
     private final HoaDonService hoaDonService;
 
     public HoaHonController(HoaDonService hoaDonService) {
         this.hoaDonService = hoaDonService;
     }
-     @PostMapping("/{id}")
+
+    @PostMapping("/{id}")
     public ResponseEntity<RestResponse> createHoaDon(@PathVariable("id") Long id) {
         RestResponse res = new RestResponse();
         res.setData(hoaDonService.create(id));
         return ResponseEntity.status(HttpStatus.CREATED).body(res);
+    }
+
+    @GetMapping()
+    public ResponseEntity<ResultPagination> getHoaDon(
+
+            @Filter Specification<Hoadon> spec, Pageable pageable) {
+
+        return ResponseEntity.ok(this.hoaDonService.getAllHoaDon(spec, pageable));
     }
 }
--- src/main/java/com/manage/quanlykytucxa/service/HoaDonService.java
+++ src/main/java/com/manage/quanlykytucxa/service/HoaDonService.java
@@ -1,9 +1,14 @@
 package com.manage.quanlykytucxa.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import com.manage.quanlykytucxa.domain.Hoadon;
 import com.manage.quanlykytucxa.domain.SoDienNuoc;
+import com.manage.quanlykytucxa.domain.User;
+import com.manage.quanlykytucxa.domain.response.ResultPagination;
 import com.manage.quanlykytucxa.repository.HoadonRepository;
 import com.manage.quanlykytucxa.repository.SoDienNuocRepository;
 import com.manage.quanlykytucxa.util.constant.InvoiceEnum;
@@ -13,24 +18,43 @@ public class HoaDonService {
 
     private final HoadonRepository hoadonRepository;
     private final SoDienNuocRepository soDienNuocRepository;
-    public HoaDonService(HoadonRepository hoadonRepository,SoDienNuocRepository soDienNuocRepository) {
+
+    public HoaDonService(HoadonRepository hoadonRepository, SoDienNuocRepository soDienNuocRepository) {
         this.hoadonRepository = hoadonRepository;
-        this.soDienNuocRepository=soDienNuocRepository;
+        this.soDienNuocRepository = soDienNuocRepository;
     }
 
     public Hoadon create(Long soDienNuocId) {
-SoDienNuoc soDn = this.soDienNuocRepository.findById(soDienNuocId).orElse(null);
-if (soDn!=null) {
-        Hoadon hoadon = new Hoadon();
-        hoadon.setSoDien(soDn.getSoDienCuoi() - soDn.getSoDienDau());
-        hoadon.setSoNuoc(soDn.getSoNuocCuoi() - soDn.getSoNuocDau());
-        hoadon.setTienDien(hoadon.getSoDien() * 3000);
-        hoadon.setTienNuoc(hoadon.getSoNuoc() * 2000);
-        hoadon.setTongTien(hoadon.getTienDien() + hoadon.getTienNuoc());
-        hoadon.setTrangThai(InvoiceEnum.CHUADONG);
-        hoadon.setRoom(soDn.getRoom());
-        return hoadonRepository.save(hoadon);
+        SoDienNuoc soDn = this.soDienNuocRepository.findById(soDienNuocId).orElse(null);
+        if (soDn != null) {
+            Hoadon hoadon = new Hoadon();
+            hoadon.setSoDien(soDn.getSoDienCuoi() - soDn.getSoDienDau());
+            hoadon.setSoNuoc(soDn.getSoNuocCuoi() - soDn.getSoNuocDau());
+            hoadon.setTienDien(hoadon.getSoDien() * 3000);
+            hoadon.setTienNuoc(hoadon.getSoNuoc() * 2000);
+            hoadon.setTongTien(hoadon.getTienDien() + hoadon.getTienNuoc());
+            hoadon.setTrangThai(InvoiceEnum.CHUADONG);
+            hoadon.setRoom(soDn.getRoom());
+            return hoadonRepository.save(hoadon);
+        }
+        return null;
     }
-    return null;
-}
+
+    public ResultPagination getAllHoaDon(Specification<Hoadon> spec, Pageable pageable) {
+
+        Page<Hoadon> pageUser = this.hoadonRepository.findAll(spec, pageable);
+
+        ResultPagination rs = new ResultPagination();
+
+        ResultPagination.Meta mt = new ResultPagination.Meta();
+        mt.setPage(pageUser.getNumber() + 1);
+        mt.setPageSize(pageUser.getSize());
+        mt.setPages(pageUser.getTotalPages());
+        mt.setTotal(pageUser.getTotalElements());
+        rs.setMeta(mt);
+        rs.setResult(pageUser.getContent());
+
+        return rs;
+    }
+
 }
