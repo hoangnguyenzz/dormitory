e18c9e094d9988bc16ae585fddc16392c18cce0d
hoangnguyenzz <nguyenvanhoang2004bn@gmail.com>
2025-04-24 08:46:17 +0700

    update-2

--- src/main/java/com/manage/quanlykytucxa/config/CorsConfig.java
+++ src/main/java/com/manage/quanlykytucxa/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(
-                Arrays.asList("http://127.0.0.1:5501", "http://localhost:4173", "http://localhost:5173"));
+                Arrays.asList("http://127.0.0.1:5501","http://127.0.0.1:5502","http://dormitory-fe.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Allowed methods
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept", "x-no-retry"));
         configuration.setAllowCredentials(true);
